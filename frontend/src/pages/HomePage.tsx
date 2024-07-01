d40ccb21db53f2fe162ab8f2078e11da24336f21
chriswike <chriswike@gmail.com>
2024-07-01 02:41:35 -0500

    pushed to dev branch

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+const HomePage = () => {
+  return(
+    <div className="flex flex-col gap-12">
+        <div className="bg-white rounded-lg shadow-md py-8 flex-col gap-5 text-center -mt-16">
+            <h1 className="text-5x1 font-bold tracking-tight text-orange-600">
+            A MERN Food Delivery Project
+            </h1>
+            <span className="text-x1">Built Using React, Node.js, MongoDB, Express</span>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage;
